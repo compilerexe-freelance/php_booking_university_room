130a46a85cec6324d6e765193bde718d483e8835
Me <compilerexe@gmail.com>
2016-11-25 23:05:02 +0700

    Design home page.

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="assets/css/bootstrap-flex.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fullcalendar-3.0.1/fullcalendar.min.css">
+    <script src="assets/js/jquery-3.1.1.min.js"></script>
+    <script src="assets/js/moment.min.js"></script>
+    <script src="assets/fullcalendar-3.0.1/fullcalendar.min.js"></script>
+    <script src="assets/fullcalendar-3.0.1/locale-all.js"></script>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,91 @@
+<?php include('header.php'); ?>
+
+  <style>
+    table, tr, th {
+      border-top: 0px !important;
+      border-bottom: 0px !important;
+    }
+    table, tr, td {
+      border-top: 0px !important;
+      border-bottom: 0px !important;
+    }
+  </style>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md" style="padding: 0px 0px 0px 0px;">
+        <img src="" alt="" style="width: 100%; height: 250px; background-color: #abc;">
+      </div>
+    </div>
+    <div class="row" style="margin-top: 20px; margin-bottom: 20px;">
+
+      <div class="col-md-3" style="border: 1px solid #abc;">
+        <button type="button" class="btn btn-secondary" style="width: 100%; margin-top: 10px; margin-bottom: 5px;">ปฏิทินการจอง</button>
+        <button type="button" class="btn btn-secondary" style="width: 100%; margin-bottom: 5px;">รายละเอียดสถานที่</button>
+        <button type="button" class="btn btn-secondary" style="width: 100%; margin-bottom: 5px;">รายละเอียดสถานที่</button>
+        <button type="button" class="btn btn-secondary" style="width: 100%; margin-bottom: 5px;">แบบประเมินความพึ่งพอใจ</button>
+        <button type="button" class="btn btn-secondary" style="width: 100%; margin-bottom: 5px;">สมัครสมาชิก</button>
+        <button type="button" class="btn btn-secondary" style="width: 100%; margin-bottom: 10px;">ลืมรหัสผ่าน</button>
+
+        <hr>
+
+        <div style="text-align: center; margin-top: 10px;">
+          <span>ผู้ดูแลระบบ</span>
+        </div>
+
+        <table class="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="align-middle">Username</td>
+              <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+              <td class="align-middle">Password</td>
+              <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><button type="button" class="btn btn-success" style="width: 100%;">เข้าสู่ระบบ</button></td>
+            </tr>
+          </tbody>
+        </table>
+
+        <hr>
+
+      </div>
+
+      <div class="col-md">
+        <span style="color: red;">* หมายเหตุ</span>&ensp;
+        <img style="width: 15px; height:15px; background-color: #59b300;">&ensp;
+        <span>รายการจองที่ผ่านการอนุมัติ</span>&ensp;
+        <img style="width: 15px; height:15px; background-color: #ffff33;">&ensp;
+        <span>รายการจองที่รอการอนุมัติ</span>
+
+        <div id='calendar' style="margin-top: 20px;"></div>
+
+      </div>
+    </div>
+  </div>
+
+  <script>
+      $(document).ready(function() {
+
+        $('#calendar').fullCalendar({
+            locale: 'th',
+            events: [
+                // {
+                //     start: '2016-11-04'
+                // }
+            ],
+            eventColor: '#59b300'
+        })
+
+      });
+  </script>
+<?php include('footer.php'); ?>
