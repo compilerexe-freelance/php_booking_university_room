172d22372313b2f3497ab6deb8ca749dc50157dc
Me <compilerexe@gmail.com>
2016-11-26 09:57:13 +0700

    Design register page.

--- register.php
+++ register.php
@@ -25,7 +25,81 @@
       <?php include('components/aside_left.php'); ?>
 
       <div class="col-md">
-
+        <table class="table">
+          <thead>
+            <tr>
+              <th><span style="font-size: 24px; font-weight: bold;">สมัครสมาชิก</span></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><span>รหัสบัตรประชาชน</span></td>
+              <td><input type="text" class="form-control"></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><span>ชื่อ</span></td>
+              <td><input type="text" class="form-control"></td>
+              <td>นามสกุล</td>
+              <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+              <td><span>หน่วยงาน</span></td>
+              <td><input type="text" class="form-control"></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><span>อาชีพ</span></td>
+              <td><input type="text" class="form-control"></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><span>ตำแหน่ง</span></td>
+              <td><input type="text" class="form-control"></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><span>ที่อยู่</span></td>
+              <td><textarea name="name" rows="8" cols="40" class="form-control" style="resize: none;"></textarea></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><span>เบอร์โทรศัพท์</span></td>
+              <td><input type="text" class="form-control"></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><span>ชื่อเข้าใช้งาน</span></td>
+              <td><input type="text" class="form-control"></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><span>รหัสผ่าน</span></td>
+              <td><input type="text" class="form-control"></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td style="text-align: right;">
+                <button type="button" class="btn btn-success">ตกลง</button>
+                <button type="button" class="btn btn-danger">ยกเลิก</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
